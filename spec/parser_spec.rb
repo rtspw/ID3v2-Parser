133f363dfb951e4df13cc84cbad26825cf4d4f91
rtspw <richard.cx.tang@gmail.com>
2021-12-13 16:54:18 -0800

    Add spec for extended header parsing

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,6 +6,71 @@ describe Parser do
       file_name = File.join(File.dirname(__FILE__), "binaries", "no_header.mp3")
       file_handle = File.open file_name
       expect { Parser::parse(file_handle) }.to raise_error(BadFormatException)
+      file_handle.close
+    end
+  end
+
+  context "when unsync flag is set" do
+    it "should throw UnsupportedException" do
+      file_name = File.join(File.dirname(__FILE__), "binaries", "unsync_flag_on.mp3")
+      file_handle = File.open file_name
+      expect { Parser::parse(file_handle) }.to raise_error(UnsupportedException)
+      file_handle.close
+    end
+  end
+
+  context "when header is well formatted" do
+    it "should correctly parse header" do
+      file_name = File.join(File.dirname(__FILE__), "binaries", "short_header.mp3")
+      file_handle = File.open file_name
+      expect(Parser::parse_header(file_handle)).to eq(
+        :file_id => 'ID3',
+        :major_version => 3,
+        :revision_number => 0,
+        :flags => {
+          :unsync => 0,
+          :extended_header => 0,
+          :experimental => 0,
+        },
+        :tag_size => 60,
+      )
+      file_handle.close
+    end
+  end
+
+  context "when it includes extended header" do
+    it "should throw if header size is invalid" do
+      file_name_without_crc = File.join(File.dirname(__FILE__), "binaries", "extended_header_bad_size.mp3")
+      file_handle = File.open file_name_without_crc
+      expect { Parser::parse(file_handle) }.to raise_error(BadFormatException)
+      file_handle.close
+    end
+    it "should correctly parse extended header when it doesn't include crc" do
+      file_name_without_crc = File.join(File.dirname(__FILE__), "binaries", "extended_header_no_crc.mp3")
+      file_handle = File.open file_name_without_crc
+      expect(Parser::parse_header(file_handle).flags.extended_header).to eq(1)
+      expect(Parser::parse_extended_header(file_handle)).to eq(
+        :extended_header_size => 6,
+        :flags => {
+          :crc_present => 0,
+        },
+        :padding_size => 0,
+      )
+      file_handle.close
+    end
+    it "should correctly parse extended header when it includes crc" do
+      file_name_with_crc = File.join(File.dirname(__FILE__), "binaries", "extended_header_crc.mp3")
+      file_handle = File.open file_name_with_crc
+      expect(Parser::parse_header(file_handle).flags.extended_header).to eq(1)
+      expect(Parser::parse_extended_header(file_handle)).to eq(
+        :extended_header_size => 10,
+        :flags => {
+          :crc_present => 1,
+        },
+        :padding_size => 0,
+        :crc => 0x12345678,
+      )
+      file_handle.close
     end
   end
 end
